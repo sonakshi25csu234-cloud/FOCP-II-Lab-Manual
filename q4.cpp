3dce4dad29e4b9a62298255233276ece34b9ab78
sonakshi25csu234-cloud <sonakshi25csu234@ncuindia.edu>
2026-05-15 10:54:37 +0530

    Create q4.cpp

---
+++ q4.cpp
@@ -0,0 +1,15 @@
+int item_no, quantity, unit_price, total_bill;
+
+  cout<<"Enter item no: ";
+  cin>>item_no;
+  cout<<"\n";
+
+  cout<<"Enter quantity: ";
+  cin>>quantity;
+  cout<<"\n";
+
+  cout<<"Enter unit_price: ";
+  cin>>unit_price;
+  cout<<"\n";
+
+  cout<<"Total bill: "<<quantity*unit_price*0.20;
