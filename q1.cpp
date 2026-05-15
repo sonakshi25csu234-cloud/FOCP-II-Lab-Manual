49317df1d6fb137ceae2edebeef8263840bfe68f
sonakshi25csu234-cloud <sonakshi25csu234@ncuindia.edu>
2026-05-15 10:52:19 +0530

    Create q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,27 @@
+float marks1, marks2, marks3, marks4, marks5, average;
+
+    // cout<<"Enter marks1" <<"\n" <<"Enter marks2"<<"\n" <<"Enter marks2"<<"\n" <<"Enter marks2"<<"\n" <<"Enter marks2";
+
+    cout<<"Enter marks1: ";
+    cin>>marks1;
+    cout<<"\n";
+
+    cout<<"Enter marks2: ";
+    cin>>marks2;
+    cout<<"\n";
+
+    cout<<"Enter marks3: ";
+    cin>>marks3;
+    cout<<"\n";
+
+    cout<<"Enter marks4: ";
+    cin>>marks4;
+    cout<<"\n";
+
+    cout<<"Enter marks5: ";
+    cin>>marks5;
+    cout<<"\n";
+
+    // average = (marks1+marks2+marks3+marks4+marks5)/5;
+
+    cout<<"Average of all marks is "<<(marks1+marks2+marks3+marks4+marks5)/5; // OR <<average;
