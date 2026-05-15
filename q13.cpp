c64bc0944b60b3ca0b7142da9aea7e91b4c5a1b6
sonakshi25csu234-cloud <sonakshi25csu234@ncuindia.edu>
2026-05-15 10:58:24 +0530

    Create q13.cpp

---
+++ q13.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter a: ";
+    cin>>a;
+    cout<<"Enter b: ";
+    cin>>b;
+    cout<<"Enter c: ";
+    cin>>c;
+    int d = b*b - 4*a*c;
+    if(d == 0){
+
+    }
+    return 0;
+}
