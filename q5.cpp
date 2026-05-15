4f4651544c6ecf654a10de2d87a1cd9a2db6b15c
sonakshi25csu234-cloud <sonakshi25csu234@ncuindia.edu>
2026-05-15 10:55:14 +0530

    Create q5.cpp

---
+++ q5.cpp
@@ -0,0 +1,32 @@
+int a, b, c;
+
+    cout<<"Enter value of a: ";
+    cin>> a;
+
+    cout<<"\n Enter value of b: ";
+    cin>> b;
+
+    // c=a;
+    // a=b;
+    // b=c;
+
+    // OR
+
+    // a = a + b;
+    // b = a - b;
+    // a = a - b;
+
+    // ORR
+
+    // a = a * b;
+    // b = a / b;
+    // a = a / b;
+
+    // ORRR
+
+    // swap(a,b);
+
+
+    cout<<"Values after swapping are: ";
+    cout<< "a = " << a;
+    cout<< " b = " << b;
